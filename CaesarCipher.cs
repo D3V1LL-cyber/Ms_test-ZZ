dd54f74ae1eef02064380ea0665436f75318dbb5
Yrii <hiroesmen@gmail.com>
2025-03-20 11:18:52 +0300

    Добавьте файлы проекта.

---
+++ CaesarCipher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipherApp
+{
+    class CaesarCipher
+    {
+        public string Encrypt(string text, int shift)
+        {
+            string result = "";
+
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    char d = char.IsUpper(c) ? 'A' : 'a';
+                    result += (char)((((c + shift) - d + 26) % 26) + d);
+                }
+                else
+                {
+                    result += c; // Не шифруем символы, которые не буквы
+                }
+            }
+            return result;
+        }
+
+        public string Decrypt(string text, int shift)
+        {
+            return Encrypt(text, 26 - (shift % 26));
+        }
+    }
+}
